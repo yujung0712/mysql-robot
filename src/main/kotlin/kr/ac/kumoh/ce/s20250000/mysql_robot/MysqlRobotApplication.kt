966de23e7db1418a84f9461babbe292ebc34f115
yujung0712 <ghddbwjd22@naver.com>
2025-05-28 19:04:37 +0900

    🎉 데이터베이스 연결 성공

---
+++ src/main/kotlin/kr/ac/kumoh/ce/s20250000/mysql_robot/MysqlRobotApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.ce.s20250000.mysql_robot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MysqlRobotApplication
+
+fun main(args: Array<String>) {
+	runApplication<MysqlRobotApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/ce/s20250000/mysql_robot/MysqlRobotApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20250000.mysql_robot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MysqlRobotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
